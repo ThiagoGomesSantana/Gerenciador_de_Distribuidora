bcdd639f14f6f1a8e3b9c4968ce91204b4ad2545
Thiago G Santana <thiago.sant3031@gmail.com>
2022-09-28 16:04:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from flet import KeyboardEvent, Page, Text
 def main(page: Page):
     print(dir(page))
     page.window_maximized = True
+    page.add(Text(value = 'Alô alô'))
     page.update()
+   
 
 
 flet.app(target=main)
